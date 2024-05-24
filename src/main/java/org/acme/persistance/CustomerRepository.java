67899621fc5a4d93857491a92440d9203eb3e1f5
Raph <63296115+Raphael-97@users.noreply.github.com>
2024-05-24 05:44:42 +0200

    Add CustomerRepository

---
+++ src/main/java/org/acme/persistance/CustomerRepository.java
@@ -0,0 +1,13 @@
+package org.acme.persistance;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.acme.domain.Customer;
+
+@ApplicationScoped
+public class CustomerRepository implements PanacheRepository<Customer> {
+
+    public Customer findByFirstName(String firstName) {
+        return find("firstName", firstName).firstResult();
+    }
+}
